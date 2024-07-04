432faf3df38e8fc9cd9450c006a3ec0a13276ab0
Santiago Soler <santisoler@fastmail.com>
2024-07-04 10:46:03 -0700

    Update hashes of csv files in registry

--- src/ice_sheets/datasets.py
+++ src/ice_sheets/datasets.py
@@ -5,8 +5,8 @@ from ._version import __version__
 
 VERSION = "v" + __version__
 REGISTRY = {
-    "antarctica.csv": "e5c0f1741bc296bb54a1ec93166efc67e2e1cde8d74f1d8089bc68cf5ee1433d",
-    "greenland.csv": "8bf3ba40cd777bc5e18d247fbfbdb05c8ff454fa84059cdc3fc507b38d4e814a",
+    "antarctica.csv": "8d32a1a4b500c443981c60fc45a7126b16a9fd6d330c8107a1e584b81e841e53",
+    "greenland.csv": "0538ba7f521e5a73a70faa00d7f917a8bbdb549c94a6b823685ff3da985cb939",
 }
 PUPPY = pooch.create(
     path=pooch.os_cache("ice-sheets"),
